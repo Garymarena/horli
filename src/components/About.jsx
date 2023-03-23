1afb7f589d144fb34f224f70daa2ffadf213d2a7
Garymarena <garym.arena@gmail.com>
2023-03-23 18:25:03 -0500

    About-full

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,14 +7,41 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div>
-          <p>Acerca</p>
+        <div className="pb-8">
+          <p className="text-4x1 font-bold inline border-b-4 border-gray-500">
+            Acerca
+          </p>
         </div>
-        <p>lorem ipsum</p>
+
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          vitae nibh ac enim consequat accumsan ut et lorem. Aenean bibendum
+          aliquam dictum. Nullam hendrerit enim sed tellus maximus, in accumsan
+          turpis sodales. Proin tempus dictum augue, at imperdiet ligula
+          ullamcorper ut. Aenean ornare sem eu ultricies faucibus. Sed
+          consequat, tortor non vulputate elementum, elit risus maximus felis,
+          eu dignissim massa diam ac tellus. Nunc finibus imperdiet lectus, sit
+          amet condimentum dui. Etiam in metus bibendum, dignissim arcu sit
+          amet, hendrerit felis. In finibus tortor quis nulla eleifend euismod.
+          Integer interdum lorem et odio tempus, in pretium enim mollis. Nulla
+          sit amet viverra turpis. Phasellus eleifend convallis finibus.
+        </p>
 
         <br />
 
-        <p>More shit</p>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          vitae nibh ac enim consequat accumsan ut et lorem. Aenean bibendum
+          aliquam dictum. Nullam hendrerit enim sed tellus maximus, in accumsan
+          turpis sodales. Proin tempus dictum augue, at imperdiet ligula
+          ullamcorper ut. Aenean ornare sem eu ultricies faucibus. Sed
+          consequat, tortor non vulputate elementum, elit risus maximus felis,
+          eu dignissim massa diam ac tellus. Nunc finibus imperdiet lectus, sit
+          amet condimentum dui. Etiam in metus bibendum, dignissim arcu sit
+          amet, hendrerit felis. In finibus tortor quis nulla eleifend euismod.
+          Integer interdum lorem et odio tempus, in pretium enim mollis. Nulla
+          sit amet viverra turpis. Phasellus eleifend convallis finibus.
+        </p>
       </div>
     </div>
   );
